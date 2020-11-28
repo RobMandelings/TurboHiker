b8a4c43ea17669389c87e58c94a47b38bff74f3f
Rob Mandelings <rob@minetronic.com>
2020-11-28 10:33:36 +0100

    SFML window finally pops open

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # TurboHiker
 Advanced Programming course assignment to create a game called 'Turbo Hiker' 
+
+## Installation guide
+
+This project requires a the SFML Library you need to download yourself, for your platform and compiler.
+I have divided the installation guide in a linux and a windows part, as the installation is different for each platform.
+
+### On linux:
+
+### On windows
--- TurboHikerSFML/src/main.cpp
+++ TurboHikerSFML/src/main.cpp
@@ -1,3 +1,24 @@
-#include "turbo_lib/lib.h"
+#include <SFML/Graphics.hpp>
 
-int main() { turbo_lib::printMessage(); }
+int main()
+{
+        sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+        sf::CircleShape shape(100.f);
+        shape.setFillColor(sf::Color::Green);
+
+        while (window.isOpen())
+        {
+                sf::Event event;
+                while (window.pollEvent(event))
+                {
+                        if (event.type == sf::Event::Closed)
+                                window.close();
+                }
+
+                window.clear();
+                window.draw(shape);
+                window.display();
+        }
+
+        return 0;
+}
