20d4d306f2b83d80afa7df614fb791d477b303fd
Rob Mandelings <rob@minetronic.com>
2020-12-19 22:30:12 +0100

    Vector and Transform classes now in namespace

--- TurboHikerLib/src/turboHiker/utils/maths/Transform.h
+++ TurboHikerLib/src/turboHiker/utils/maths/Transform.h
@@ -5,8 +5,10 @@
 #ifndef TURBOHIKER_TRANSFORM_H
 #define TURBOHIKER_TRANSFORM_H
 
+namespace turboHiker {
 class Transform
 {
 };
+} // namespace turboHiker
 
 #endif // TURBOHIKER_TRANSFORM_H
--- TurboHikerLib/src/turboHiker/utils/maths/Vector2d.h
+++ TurboHikerLib/src/turboHiker/utils/maths/Vector2d.h
@@ -5,8 +5,10 @@
 #ifndef TURBOHIKER_VECTOR2D_H
 #define TURBOHIKER_VECTOR2D_H
 
+namespace turboHiker {
 class Vector2d
 {
 };
+} // namespace turboHiker
 
 #endif // TURBOHIKER_VECTOR2D_H
