43b079ca9461e0dbad246e671f7e99706b18f44e
Rob Mandelings <rob@minetronic.com>
2020-12-19 22:29:31 +0100

    Added transformation class in the TurboHiker SFML implementation

---
+++ TurboHikerSFML/src/utils/Transformation.h
@@ -0,0 +1,12 @@
+//
+// Created by RobMa on 19/12/2020.
+//
+
+#ifndef TURBOHIKER_TRANSFORMATION_H
+#define TURBOHIKER_TRANSFORMATION_H
+
+class Transformation
+{
+};
+
+#endif // TURBOHIKER_TRANSFORMATION_H
